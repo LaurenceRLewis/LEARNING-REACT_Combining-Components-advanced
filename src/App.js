aa6caedd6e4e1da73e151717916d5a4affefe5e6
Laurence Lewis (he/him) <laurence@a11ycats.com.au>
2023-02-04 15:11:08 +1000

    Relocate common Styles to the Components folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import StaticContent from "./components/StaticContent/StaticContent";
 import Email from "./components/Email/Email";
 import Name from "./components/Name/Name";
-import "../../../src/styles.css";
+import "./components/Styles/styles.css";
 
 function App() {
   return (
